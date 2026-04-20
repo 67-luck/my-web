dff5ffb7f3d6a3690510d42fe99d41f844707984
wanghao <16608407+ikeu67@user.noreply.gitee.com>
2026-04-20 12:08:28 +0800

    fix: 修复Vercel部署白屏问题

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   // 关键：把 base 改成你的仓库名，前后都要有斜杠
-  base: '/couple-love-station/',
+  base: '/',
   server: {
     port: 3000
   }
